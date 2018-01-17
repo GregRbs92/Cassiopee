0932c354ad58b20598efb4524a03550a629b9967
Greg <rabasse.greg@gmail.com>
2018-01-17 04:45:57 +0100

    design de la homepage comme doctolib

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  searchForm: FormGroup;
 
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+    this.searchForm = this.formBuilder.group({
+      doctor: ['', Validators.required],
+      speciality: [''],
+      location: ['', Validators.required]
+    })
+  }
+
+  search() {
+    console.log(this.searchForm.value);
   }
 
 }
