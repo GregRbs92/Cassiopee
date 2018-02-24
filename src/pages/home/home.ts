d66f14e829021ea913675b3bb603135781cc3f1c
Greg <rabasse.greg@gmail.com>
2018-02-24 13:48:34 +0100

    design homepage + userpage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,35 +8,17 @@ import { TranslatePage } from '../translate/translate';
 })
 export class HomePage {
 
+  appointments = [];
+
   constructor(public navCtrl: NavController) {
-  }
-
-  goTo(page:string) {
-    switch (page) {
-      case 'medical':
-        console.log("Going to medical page");
-        break;
-
-      case 'accomodation':
-        console.log("Accomodation");
-        break;
-
-      case 'translator':
-        this.navCtrl.push(TranslatePage);
-        break;
-
-      case 'services':
-        console.log("Going to services page");
-        break;
-
-      default:
-        alert("Error: This page does not exist...");
-        break;
-    }
   }
 
   goToResearch() {
     this.navCtrl.parent.select(1);
+  }
+
+  onSubmit(message) {
+    console.log(message);
   }
 
 }
